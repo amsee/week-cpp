c045135c3f20b61b50d1f25cd75acc0df023333b
Amy <17189688+amsee@users.noreply.github.com>
2020-01-09 21:44:05 -0500

    Update namespace_inout.cpp

--- namespace_inout.cpp
+++ namespace_inout.cpp
@@ -6,19 +6,19 @@ namespace apple {
     int a = 5, b = 0;
 }
 
-namespace banana {
+namespace pineapple {
     double a = 5.5, b = 0.5;
 }
 
 int main() {
 
     cout << "a from apple " << apple::a << endl;
-    cout << "a from banana " << banana::a << endl;
+    cout << "a from pineapple " << pineapple::a << endl;
 
     using apple::a;
     a++;
     cout << "a from apple after " << apple::a << endl;
 
-    { using namespace banana;
-    cout << "from banana " << a+b << endl; }
+    { using namespace pineapple;
+    cout << "from pineapple " << a+b << endl; }
 }
