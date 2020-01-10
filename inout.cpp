0dbee53b1444d658901606b2af9ddad33beb09bd
Amy Wong <amywong@Amys-MacBook-Pro.local>
2020-01-09 21:41:31 -0500

    adding files

---
+++ inout.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char name[32];
+    int age;
+
+    cout << "Enter your name: " << endl;
+    cin >> name;
+
+    cout << "Enter your age: " << endl;
+    cin >> age;
+
+    cout << "Your name is " << name << " and you are " << age << " years old" << endl;
+    
+}
---
+++ namespace_inout.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+namespace apple {
+    int a = 5, b = 0;
+}
+
+namespace banana {
+    double a=5.5, b = 0.5;
+}
+
+int main() {
+
+    cout << "a from apple " << apple::a << endl;
+    cout << "a from banana " << banana::a << endl;
+
+    using apple::a;
+    a++;
+    cout << "a from apple after " << apple::a << endl;
+
+    { using namespace banana;
+    cout << "from banana " << a+b << endl; }
+}
